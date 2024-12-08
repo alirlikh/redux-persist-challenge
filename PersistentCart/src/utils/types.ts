1002b60d04abf16462de31ec1c5799f13bb71a95
Alireza Jalili <alirlikh@gmail.com>
2024-12-08 10:19:19 +0330

    feat: create product type

--- PersistentCart/src/utils/types.ts
+++ PersistentCart/src/utils/types.ts
@@ -1,5 +1,12 @@
-export type ProductType = {
+export type Product = {
   id: number
   title: string
   price: number
+}
+
+export type ProductResponse = {
+  total: number
+  skip: number
+  limit: number
+  products: Product[]
 }
