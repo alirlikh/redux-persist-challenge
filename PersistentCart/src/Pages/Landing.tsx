5cadb2b434422be8e7062930988e33e202a5eccc
Alireza Jalili <alirlikh@gmail.com>
2024-12-08 09:54:34 +0330

    feat: landing page

--- PersistentCart/src/Pages/Landing.tsx
+++ PersistentCart/src/Pages/Landing.tsx
@@ -1,10 +1,20 @@
 import { ProductList } from "../Componenet"
 
 const Landing = () => {
+  const addToChart = () => {
+    console.log("add to cart")
+  }
   return (
-    <main>
-      <ProductList />
-    </main>
+    <>
+      <header style={{ margin: "10px 0" }}>
+        <nav style={{ backgroundColor: "grey", height: "30px", padding: "10px" }}>
+          <button onClick={addToChart}>cart</button>
+        </nav>
+      </header>
+      <main>
+        <ProductList />
+      </main>
+    </>
   )
 }
 export default Landing
