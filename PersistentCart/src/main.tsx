a526f3490b7d402f00e831aae2cc9937ab79b1e9
Alireza Jalili <alirlikh@gmail.com>
2024-12-30 10:25:15 +0330

    feat: add loading to persistor

--- PersistentCart/src/main.tsx
+++ PersistentCart/src/main.tsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
+        <PersistGate loading={<>loading...</>} persistor={persistor}>
           <App />
         </PersistGate>
       </Provider>
